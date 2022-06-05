31cb18f025979e0cd327408ad9346a13c79082ce
Alina <lewibein@icloud.com>
2022-06-05 21:27:37 +0300

    add example 005

---
+++ example_005_ifelse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите  Ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+
+{
+    Console.WriteLine("Иди нахуй,Маша");
+}
+
+else
+
+{
+    Console.Write("Дарова ");
+    Console.WriteLine(username);
+}
