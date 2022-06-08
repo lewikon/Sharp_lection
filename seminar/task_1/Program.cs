b4257005f0b30ce201d55eb219dd830b7f744e61
Alina <lewibein@icloud.com>
2022-06-08 20:12:31 +0300

    dog_task

---
+++ seminar/task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int count = 0;
+double distance = 10000;
+int meetDistance = 10;
+bool friends = true;
+
+while(distance > meetDistance)
+{
+    double time = 0;
+    if(friends == true)
+    {
+        time = distance/(FirstFriendSpeed + DogSpeed);
+        friends = false;
+    }
+    else
+    {
+        time = distance/(SecondFriendSpeed + DogSpeed);
+        friends = true;
+    }
+    distance = distance - time  * (FirstFriendSpeed + SecondFriendSpeed);
+    count++;
+}
+Console.WriteLine(count);
+
