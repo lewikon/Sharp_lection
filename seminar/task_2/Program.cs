b97e5857e7d355957c8d731d75b364943b92fc4b
Alina <lewibein@icloud.com>
2022-06-08 20:33:14 +0300

    square

---
+++ seminar/task_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число:");
+int number =  Convert.ToInt32(Console.ReadLine()); 
+//Преобразует строки в 32-битовое целое числосо знаком: Convert.ToInt32 
+//Cчитывает информацию введеную с терминала:(Console.ReadLine())
+int result = number * number;
+Console.WriteLine("Квадрат числа:" + result);
