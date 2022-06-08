9d70d3b4344759572aeb531d2a1c453121f7d51e
Alina <lewibein@icloud.com>
2022-06-08 21:51:20 +0300

    elseifsquare

---
+++ seminar/task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите меньшее число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите большее число:");
+int square = Convert.ToInt32(Console.ReadLine());
+int result = square / number;
+if(number == result)
+{
+    Console.WriteLine("Число " + square + " является квадратом числа " + number);
+}
+else
+{
+    Console.WriteLine("Число " + square + " не является квадратом числа " + number);
+}
