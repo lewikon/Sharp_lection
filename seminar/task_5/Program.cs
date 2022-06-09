cedae4a930cf32ebe8b7302a15d7eee48c9908a1
Alina <lewibein@icloud.com>
2022-06-09 22:22:59 +0300

    reverse numbers

---
+++ seminar/task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите цифру:");
+int number = Convert.ToInt32(Console.ReadLine());
+int positiveNumber = number * -1;
+
+if(number > 0)
+while (positiveNumber < number+1)
+    {
+        Console.WriteLine(positiveNumber);
+        positiveNumber++;  
+    } 
+else if (number < 0)
+{
+    do
+    {
+        Console.WriteLine(positiveNumber); 
+        positiveNumber--;
+    } 
+    while (positiveNumber > number-1);
+}
