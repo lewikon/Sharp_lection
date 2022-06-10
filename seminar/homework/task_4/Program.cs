51a6987038f717c710eba9eb5acf1effa3a4a968
Alina <lewibein@icloud.com>
2022-06-10 19:59:14 +0300

    even numbers

---
+++ seminar/homework/task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 8: Напишите программу, 
+которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+if(number < 1)
+{
+  Console.WriteLine("Введите целое число больше 1");
+}
+else
+{
+    while(a <= number)
+    {
+        if(a % 2 == 0)
+        {
+        Console.Write(a + "; ");
+        }
+        a++;
+    }
+}
