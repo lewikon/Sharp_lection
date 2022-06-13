eb62cb68c7291d6884858f4eb18c2526411653c5
Alina <lewibein@icloud.com>
2022-06-13 18:38:36 +0300

    find index

---
+++ lection_2/example_003_indexfind/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array={1,4,76,28,47,284,74,184,4};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
