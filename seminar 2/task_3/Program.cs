2e81fe38cb524b843741197b80ac36c79b2a1643
Alina <lewibein@icloud.com>
2022-06-13 22:01:22 +0300

    % or yes

---
+++ seminar 2/task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber % secondNumber == 0)
+{
+    Console.WriteLine("Число " + firstNumber + " кратно " + secondNumber);
+}
+else
+{
+    int lost = firstNumber % secondNumber;
+    Console.WriteLine("Остаток деления: " + lost);
+}
