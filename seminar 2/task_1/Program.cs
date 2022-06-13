de9cf3620355a2d9a24fbe544bce2ef31b75521f
Alina <lewibein@icloud.com>
2022-06-13 21:30:59 +0300

    example methods

---
+++ seminar 2/task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetRandomNumber()
+{
+    return new Random().Next(10, 1000);
+}
+int FindMaxDigit(int number)
+{
+int max = int.MinValue;
+while(number >  0)
+{
+    int lastDigit = number % 10;
+    if(lastDigit > max)
+    {
+        max = lastDigit;
+    }
+    number = number / 10;
+}
+return max;
+}
+
+int number = GetRandomNumber();
+int maxDigit = FindMaxDigit(number);
+
+Console.WriteLine(number);
+Console.WriteLine(maxDigit);
