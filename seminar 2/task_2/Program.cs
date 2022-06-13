b81c3b2e950f751507416422089dd61f13eaac63
Alina <lewibein@icloud.com>
2022-06-13 21:53:42 +0300

    422  - 42

---
+++ seminar 2/task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100,1000);
+int firstDigit = number % 10;
+int secondDigit = number / 100;
+
+Console.WriteLine(number);
+Console.WriteLine(secondDigit + "" + firstDigit);
+
