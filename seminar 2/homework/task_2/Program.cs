1de1e9c98edcf6cbcc10002572c2a278f0ddc76e
Alina <lewibein@icloud.com>
2022-06-14 22:32:38 +0300

    third number

---
+++ seminar 2/homework/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(number > 1000)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine(number);
+}
