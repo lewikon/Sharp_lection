94be6e29e0c9813c5c86ebd410f79f36a69826aa
Alina <lewibein@icloud.com>
2022-06-14 23:03:56 +0300

    square

---
+++ seminar 2/homework/task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+if(firstnumber * firstnumber == secondnumber)
+{
+    Console.WriteLine("Число " + secondnumber + " является квадратом " + firstnumber);
+}
+else if(secondnumber * secondnumber == firstnumber)
+{
+    Console.WriteLine("Число " + firstnumber + " является квадратом " + secondnumber);
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
