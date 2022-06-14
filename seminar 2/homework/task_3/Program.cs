392d9a4b5e4ce8c7aec53e713f69b505789a83b9
Alina <lewibein@icloud.com>
2022-06-14 22:40:32 +0300

    week days

---
+++ seminar 2/homework/task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day > 7 || day < 1)
+{
+    Console.WriteLine("в неделе семь дней");
+}
+else
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        Console.WriteLine("Это рабочий день");
+    }
+}
