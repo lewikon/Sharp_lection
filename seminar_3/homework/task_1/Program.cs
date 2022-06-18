b30be0247f1c963f7b3d8ba010aa59198c1486e3
Alina <lewibein@icloud.com>
2022-06-18 12:23:12 +0300

    pal

---
+++ seminar_3/homework/task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int Input(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Input("Введите пятизначное число:");
+if(number < 10000 || number > 99999)
+{
+    Console.WriteLine("Введите пятизначное число");
+    return;
+}
+int number5 = number % 10;
+int number4 = (number / 10) % 10;
+int number2 = (number / 1000) % 10;
+int number1 = (number / 10000) % 10;
+if(number5 == number1  && number2 == number4)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
