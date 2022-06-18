4e86e9ff6ba5cc145692cbc6f6042b16a5d7a65e
Alina <lewibein@icloud.com>
2022-06-18 09:02:21 +0300

    quarter

---
+++ seminar_3/task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите координату по x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату по y");
+int y = Convert.ToInt32(Console.ReadLine());
+if( x  == 0 &&  y == 0)
+{
+    Console.WriteLine("x и y не могут быть равны нулю");
+    return;
+}
+int result = FindNumberQuarter(x,y);
+Console.WriteLine($"Номер четверти:{result}");
+
+int FindNumberQuarter(int x, int y)
+{
+    int result = 0;
+if( x > 0 && y > 0)
+{
+   result = 1;
+}
+if( x < 0 && y < 0)
+{
+    result = 3;
+}
+if( x > 0 && y < 0)
+{
+    result = 4;
+}
+if( x < 0 && y > 0)
+{
+    result = 2;
+}
+return result;
+}
