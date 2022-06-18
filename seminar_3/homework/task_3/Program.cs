7f8073e835fc87f97aa60c0d8c3a2fcc28f19967
Alina <lewibein@icloud.com>
2022-06-18 10:58:02 +0300

    cube

---
+++ seminar_3/homework/task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondnumber = 1;
+int cube = 0;
+while (secondnumber <= number)
+{
+    cube = secondnumber*secondnumber*secondnumber;
+    Console.WriteLine(cube);
+    secondnumber++;
+}
