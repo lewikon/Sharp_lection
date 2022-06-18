0b9824b561b9642e1b49173e2b0d008961cec74e
Alina <lewibein@icloud.com>
2022-06-18 10:55:01 +0300

    vector

---
+++ seminar_3/homework/task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Координату х первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координату y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координату z первой точки");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координату х второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координату у второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координату z второй точки");
+int z2 = Convert.ToInt32(Console.ReadLine());
+int distX = x1 - x2;
+int distY = y1 - y2;
+int distZ = z1 - z2;
+double vector = Math.Sqrt(distX*distX + distY*distY + distZ*distZ);
+Console.WriteLine($"Расстояние между точками:{vector}");
