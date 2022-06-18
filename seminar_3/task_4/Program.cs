2d208f4b2ce77de1dd44acab344a34949dd4b8cb
Alina <lewibein@icloud.com>
2022-06-18 10:10:13 +0300

    square from1

---
+++ seminar_3/task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondnumber = 1;
+int square = 0;
+while (secondnumber <= number)
+{
+    square = secondnumber*secondnumber;
+    Console.WriteLine(square);
+    secondnumber++;
+}
+
