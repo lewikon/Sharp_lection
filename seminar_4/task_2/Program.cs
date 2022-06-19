1e566b8c1a489bd33c380fbaa57bf136541982bd
Alina <lewibein@icloud.com>
2022-06-19 19:39:05 +0300

    number  count  while

---
+++ seminar_4/task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count =0;
+while (number >= 1)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine(count);
