6195595a9b8a8b87ebfb370807210cfd89dc3611
Alina <lewibein@icloud.com>
2022-06-19 19:46:09 +0300

    accu for

---
+++ seminar_4/task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int accumulation = 1;
+for(int i = 1; i <= number; i++)
+{
+    accumulation = accumulation * i;
+}
+Console.WriteLine(accumulation);
