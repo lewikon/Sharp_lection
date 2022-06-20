ace2bb86be8de0631688fb40087c10db8f72daba
Alina <lewibein@icloud.com>
2022-06-20 19:45:54 +0300

    array random

---
+++ seminar_4/homework/task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int [8];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100);
+}
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
