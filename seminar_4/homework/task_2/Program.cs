29a1f03551e1eac265cff4c3b71342d781bd4fd9
Alina <lewibein@icloud.com>
2022-06-20 18:56:36 +0300

    sum

---
+++ seminar_4/homework/task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int Input(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Input("Введите число");
+int sum = 0;
+while(number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine(sum);
