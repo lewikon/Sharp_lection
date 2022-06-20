3ab2633583c195d82407729c6af91508fd3a50eb
Alina <lewibein@icloud.com>
2022-06-20 18:45:37 +0300

    power

---
+++ seminar_4/homework/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Input(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = Input("Введите число:");
+int b = Input("Введите число:");
+int power = 1;
+for(int i = 1; i <= b; i++)
+{
+    power = power*a;
+}
+Console.WriteLine(power);
