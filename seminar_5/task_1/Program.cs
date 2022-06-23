2dffe63d9d2905682c2c39f051d67c4b0fa6e8e9
Alina <lewibein@icloud.com>
2022-06-23 21:07:28 +0300

    reverse array

---
+++ seminar_5/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[10];
+int[] reverseArray = new int[10];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-20,20);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    reverseArray[i] = array[i] * -1;
+    Console.Write(reverseArray[i] + " ");
+}
