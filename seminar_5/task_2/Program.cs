2bec2daee6085ff29928faf4d16a843a5f6e2473
Alina <lewibein@icloud.com>
2022-06-23 21:27:45 +0300

    find number

---
+++ seminar_5/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[10];
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] == number || array[i] * -1 == number)
+    {
+        count++;
+        Console.WriteLine("да");
+        break;
+    }
+}
+if(count == 0)
+{
+   Console.WriteLine("нет"); 
+}
+
