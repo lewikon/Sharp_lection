da0ac7c59691dbc36ec55292c6ffa74d0ddcf544
Alina <lewibein@icloud.com>
2022-06-25 22:04:40 +0300

    into binare

---
+++ seminar_6/task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int convertedNumber = 0;
+string reverseNumber = string.Empty;
+while(number > 0)
+{
+    convertedNumber = number % 2;
+    number = number / 2;
+    reverseNumber = reverseNumber + convertedNumber;
+}
+
+for(int i = reverseNumber.Length - 1; i >= 0; i--)
+{
+    Console.Write(reverseNumber[i]);
+}
