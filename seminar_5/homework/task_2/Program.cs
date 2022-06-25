09a1b9b712012078c591fb621c7c8fe11149a158
Alina <lewibein@icloud.com>
2022-06-25 14:56:53 +0300

    sum

---
+++ seminar_5/homework/task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 36: Задайте одномерный массив, 
+заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+int[] array = new int[10];
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for(int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine(sum);
