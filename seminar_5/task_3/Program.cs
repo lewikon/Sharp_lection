91188d7ff8741d41b0c02708ecee150db7b71915
Alina <lewibein@icloud.com>
2022-06-25 14:34:00 +0300

    123 number find interval

---
+++ seminar_5/task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[123];
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 9 && array[i] < 100)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
