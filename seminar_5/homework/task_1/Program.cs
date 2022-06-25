3204d1994498e44137effcf5d2b26a2d6291d338
Alina <lewibein@icloud.com>
2022-06-25 14:50:15 +0300

    %2  == 0

---
+++ seminar_5/homework/task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[10];
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
