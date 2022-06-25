279f62f41b1053b9212df98fc19dae0d1d968807
Alina <lewibein@icloud.com>
2022-06-25 19:45:08 +0300

    revers

--- seminar_6/task_1/Program.cs
+++ seminar_6/task_1/Program.cs
@@ -1,13 +1,17 @@
 ﻿int[] numbers = {1, 2, 3, 4, 5, 6};
+int[] reverseArray = new int[numbers.Length];
 
-for(int i = 0; i < numbers.Length / 2; i++)
+/*for(int i = 0; i < numbers.Length / 2; i++)
 {
     int temp = numbers[i];
     numbers [i] = numbers[numbers.Length - 1 - i];
     numbers[numbers.Length - 1 - i] = temp;
+}*/
+for(int i = 0; i < reverseArray.Length; i++)
+{
+    reverseArray[i] = numbers[numbers.Length - 1 - i];
 }
-
 for(int i = 0; i < numbers.Length; i++)
 {
-    Console.Write(numbers[i] + " ");
+    Console.Write(reverseArray[i] + " ");
 }
