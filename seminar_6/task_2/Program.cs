3106ee0e29495e5bca24f6fe7c190c38829e7a99
Alina <lewibein@icloud.com>
2022-06-25 20:00:34 +0300

    triagle

---
+++ seminar_6/task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//кажда сторона треугольника меньше суммы двух других
+Console.WriteLine("Введите первую сторону: ");
+int firstSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону: ");
+int secondSide = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону: ");
+int thirdSide = Convert.ToInt32(Console.ReadLine());
+if(firstSide < secondSide + thirdSide && secondSide < firstSide + thirdSide && thirdSide < firstSide +secondSide)
+{
+    Console.WriteLine("Такой треугольник существует");
+}
+else
+{
+    Console.WriteLine("Такой треугольник не существует");
+}
