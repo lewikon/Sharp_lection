645d58120410491b99c75aa98992eb199fdc8e3c
Alina <lewibein@icloud.com>
2022-06-26 08:49:15 +0300

    copy

---
+++ seminar_6/task_5/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = new int[10];
+int[] copyArray = new int[array.Length];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10,10);
+}
+for(int i = 0; i < copyArray.Length; i++)
+{
+    copyArray[i] = array[i];
+}
+PrintArray(array);
+Console.WriteLine();
+PrintArray(copyArray);
+
+
+void PrintArray(int[] copyArray)
+{
+for(int i = 0; i < copyArray.Length; i++)
+{
+    Console.Write(copyArray[i] + ",");
+}
+}
