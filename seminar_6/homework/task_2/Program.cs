5523613d06610450a775e2e8793b27bd4fbad99b
Alina <lewibein@icloud.com>
2022-06-26 21:45:38 +0300

    point of two lines

---
+++ seminar_6/homework/task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите коэффициент х первого уравнения: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент х второго уравнения: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент смещения по оси у первого уравнения: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент смещения по оси у второго уравнения: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine(x + ";" + y);
+
+
+
