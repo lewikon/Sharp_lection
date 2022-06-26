21eab256240c7af4b33b559ce027c9a057631172
Alina <lewibein@icloud.com>
2022-06-26 08:35:57 +0300

    fib

---
+++ seminar_6/task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] fibbonacci = new int[size];
+fibbonacci[0] = 0;
+fibbonacci[1] = 1;
+for(int i = 2; i < fibbonacci.Length; i++)
+{
+    fibbonacci[i] = fibbonacci[i-1] + fibbonacci[i-2];
+}
+for(int i = 0; i < fibbonacci.Length; i++)
+{
+    Console.Write(fibbonacci[i]+ ",");
+}
+
