abf33baef0c950ed67b25be7b1d5ea743b432f06
Alina <lewibein@icloud.com>
2022-06-28 19:33:12 +0300

    positive number

---
+++ seminar_6/homework/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите числа через запятую: ");
+string? numbersFromUser = Console.ReadLine();
+int negativeCount = 0;
+int commaCount = 0;
+int positiveNumbers = 0;
+for(int i = 0; i < numbersFromUser.Length; i++)
+{
+    if(numbersFromUser[i] == '-') negativeCount++;
+    if(numbersFromUser[i] == ',') commaCount++;
+}
+commaCount = commaCount + 1;
+positiveNumbers = commaCount - negativeCount;
+Console.WriteLine(positiveNumbers);
