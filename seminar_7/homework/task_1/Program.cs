5bb5bbd1a18cd5f0514449e29e3620c35d0958ff
Alina <lewibein@icloud.com>
2022-07-03 20:47:32 +0300

    random

---
+++ seminar_7/homework/task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+{
+   for(int j = 0; j < array.GetLength(1); j++)
+   {
+    Console.Write(array[i,j] + " ");
+   }
+   Console.WriteLine();
+}
+}
+int m = Input("Введите m:");
+int n = Input("Введите n:");
+
+double[,] numbers = new double[m,n];
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+   for(int j = 0; j < numbers.GetLength(1); j++)
+   {
+    numbers[i,j] = new Random().Next(-10,10)  / 2.5;
+   }
+}
+PrintArray(numbers);
