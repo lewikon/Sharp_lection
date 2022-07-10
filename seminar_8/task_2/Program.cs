21fa4f232abea283aac84401fc63b4a03b1a0fa2
Alina <lewibein@icloud.com>
2022-07-10 10:12:33 +0300

    reverse table

---
+++ seminar_8/task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+{
+   for(int j = 0; j < array.GetLength(1); j++)
+   {
+    Console.Write(array[i,j]);
+   }
+   Console.WriteLine();
+}
+}
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = Input("Введите количество строк: ");
+int n = Input("Введите количество столбцов: ");
+
+int[,] numbers = new int[m,n];
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+   for(int j = 0; j < numbers.GetLength(1); j++)
+   {
+    numbers[i,j] = new Random().Next(1,10);
+   }
+}
+int[,] reverseTable =  new int[m,n];
+int temp = 0;
+PrintArray(numbers);
+Console.WriteLine();
+if(m != n)
+{
+    Console.WriteLine("Это невозможно перевернуть");
+}
+else
+{
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for(int j = 0; j < numbers.GetLength(1); j++)
+    {
+        temp = numbers[i,j];
+        reverseTable[j,i] = temp;
+    }
+}
+PrintArray(reverseTable);
+}
