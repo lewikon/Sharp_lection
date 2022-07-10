5829fd1b3bfa1e3b2c21204caf3a1dd0e2ba2806
Alina <lewibein@icloud.com>
2022-07-10 20:43:13 +0300

    step

---
+++ seminar_9/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Power(a,b));
+
+int Power(int a, int b)
+{
+    if(b == 1) return a;
+    else
+    {
+       return a * Power(a,b-1);
+    }
+}
