e1131ea637dde5a5a0781ea197a78f4b96282f85
Alina <lewibein@icloud.com>
2022-07-10 19:31:19 +0300

    rec

---
+++ seminar_9/task_1_demo/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = int.Parse(Console.ReadLine());
+Console.WriteLine(NaturalNumber(number));
+
+int NaturalNumber (int number)
+{
+    if(number ==1) return number;
+    else 
+    {
+        Console.Write($"{NaturalNumber(number -1)},");
+        return number;
+    }
+}
