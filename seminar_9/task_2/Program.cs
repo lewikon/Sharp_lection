aaee7c90668246cfa19f0ee1d8f05a2276b108de
Alina <lewibein@icloud.com>
2022-07-10 19:39:42 +0300

    first

---
+++ seminar_9/task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(NaturalNumber(m,n));
+
+int NaturalNumber (int m,int n)
+{
+    if(m == n) return m;
+    else
+    {
+        Console.Write($"{NaturalNumber(m,n-1)},");
+        return n;
+    }
+}
