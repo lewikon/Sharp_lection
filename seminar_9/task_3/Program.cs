421ac75670f1e229094aca2b76d36fa1dbee1673
Alina <lewibein@icloud.com>
2022-07-10 20:28:18 +0300

    sum

---
+++ seminar_9/task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Num(num));
+
+int Num(int num)
+{
+    if(num > 0)
+    {
+        return num % 10 + Num( num / 10);
+    }
+    return num;
+}
