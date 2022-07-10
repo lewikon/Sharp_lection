d49c4bbc99fe38d309d5c2eda26b71f6676f7a59
Alina <lewibein@icloud.com>
2022-07-10 14:00:43 +0300

    order in line

---
+++ seminar_8/homework/task_1/Program.cs
@@ -0,0 +1,48 @@
+﻿int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+{
+   for(int j = 0; j < array.GetLength(1); j++)
+   {
+    Console.Write(array[i,j] + " ");
+   }
+   Console.WriteLine();
+}
+}
+int m = Input("Введите m:");
+int n = Input("Введите n:");
+
+int[,] numbers = new int[m,n];
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+   for(int j = 0; j < numbers.GetLength(1); j++)
+   {
+    numbers[i,j] = new Random().Next(0,100);
+   }
+}
+PrintArray(numbers);
+int temp =  0;
+int lineCount = 0;
+while(lineCount < numbers.GetLength(1))
+{
+for(int i = 0; i < numbers.GetLength(0); i++)
+{
+   for(int j = 0; j < numbers.GetLength(1)-1; j++)
+   {
+    if (numbers[i,j] > numbers[i,j+1])
+    {
+        temp = numbers[i,j];
+        numbers[i,j] = numbers[i,j+1];
+        numbers[i,j+1] = temp;
+    }
+   }
+}
+lineCount++;
+}
+Console.WriteLine();
+PrintArray(numbers);
