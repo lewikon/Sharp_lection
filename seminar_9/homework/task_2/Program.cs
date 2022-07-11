d4cc03335fa77bf70f3dc2e01867aa44189007cc
Alina <lewibein@icloud.com>
2022-07-11 19:49:01 +0300

    sum

---
+++ seminar_9/homework/task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Sum(a,b));
+
+int Sum(int a, int b)
+{
+    if(b == a) return a;
+    else
+    {
+       return a + Sum(a+1,b);
+    }
+}
