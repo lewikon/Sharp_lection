a0b59de33fc4a0170418bcf0d6545d3641842b81
Alina <lewibein@icloud.com>
2022-07-11 23:12:27 +0300

    work

--- seminar_9/homework/task_1/Program.cs
+++ seminar_9/homework/task_1/Program.cs
@@ -1,26 +1,13 @@
 ﻿int number = int.Parse(Console.ReadLine());
-Reverse(Console.Write(NumbersLine(number)));
-
-int NumbersLine (int number)
+int firstNumber = 1;
+Console.Write(NumbersLine(firstNumber));
+int NumbersLine(int count)
 {
-    if(number == 1) 
-    return 1;
-    else 
+    if (count == number)
+        return count;
+    else
     {
-        return NumbersLine(number - 1);
-    }
-}
-Reverse(Console.ReadLine());
-void Reverse (char[] line)
-{   
-    int i = 0;
-    while(line[i] < line.Length/2)
-    {
-        char[] temp = new char[line.Length];
-        if (line [i] == ',') continue;
-        temp[i] = line[i];
-        line[i] = line[line.Length-i-1];
-        line[line.Length-i-1] = temp[i];
-        i++;
+        Console.Write(NumbersLine(count + 1) + ",");
+        return count;
     }
 }
