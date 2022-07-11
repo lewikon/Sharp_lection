bb5fc9adcc572dd2c7368e7b15c16c6281c6667e
Alina <lewibein@icloud.com>
2022-07-11 20:47:55 +0300

    accerman

---
+++ seminar_9/homework/task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Accerman(m,n));
+
+int Accerman(int m, int n)
+{
+    if (m == 0) return n+1;
+    else if(m > 0 && n ==0)
+    {
+        return Accerman(m - 1, 1);
+    }
+    else
+    {
+        return Accerman(m-1, Accerman(m,n-1));
+    }
+}
