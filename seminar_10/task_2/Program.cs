bcf668fb57bf455d5e133f30144a707d4f09421d
Alina <lewibein@icloud.com>
2022-07-12 21:45:41 +0300

    aisv

---
+++ seminar_10/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿string charsWords = "аисв";
+int countCharsInWords = 3;
+PrintAllWords(charsWords,countCharsInWords,"");
+
+void PrintAllWords(string alphabet, int length, string prefix)
+{
+if (length == 0)
+{
+Console.Write(prefix + " ");
+}
+else
+{
+for (int i = 0; i < alphabet.Length; i++)
+{
+PrintAllWords(alphabet,length-1,prefix + alphabet[i]);
+}
+}
+}
