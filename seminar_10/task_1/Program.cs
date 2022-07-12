6b72b6801d3b598b880d0bdba1f21962a3524998
Alina <lewibein@icloud.com>
2022-07-12 21:02:17 +0300

    ps fib

---
+++ seminar_10/task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int count = int.Parse(Console.ReadLine());
+Console.WriteLine(Sum(a,b,count));
+
+
+string Sum(int a, int b, int count)
+{
+    if(count == 0) 
+    {
+        return b + "";
+    }
+    else
+    {
+        return a + "," + Sum(b, a+b, --count);
+    }
+}
